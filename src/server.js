40db13443d0f3dff5590e037bebc68122635c8b8
Pedro <pedroh.fokus@gmail.com>
2023-10-08 18:44:30 -0300

    finished project

--- src/server.js
+++ src/server.js
@@ -1,19 +1,35 @@
-const express = require("express"); //importei o express
+require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
 
-const app = express(); //inicializei o express
+const express = require('express');
 
-// // Quando um cliente faz uma solicitação GET para a rota raiz ("/")
-app.get("/message/:id/:product", (request, response) => {
+const routes = require('./routes');
 
-const {id , product} = request.params;
+migrationsRun();
 
-// O servidor responde:
-  response.send(`
-  Bem Vindo a página de id: ${id} \n
-  Esta pagina pertence ao produto ${product}
-  `);
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+
+app.use((error,request,response,next) => {
+  if(error instanceof AppError)
+  {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
 });
 
-const PORT = 3333; //constante pra definir a porta que tera um observador
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
-//quando iniciar, executar essa mensagem no terminal
